aab2d2d8250980914c11dacd878ac012a2877daa
soulo <tsoulo@student.wethinkcode.co.za>
2023-09-01 22:50:15 +0200

    Delete document intergration

--- controllers/resources.js
+++ controllers/resources.js
@@ -32,10 +32,18 @@ module.exports.uploadDocument = async(req,res)=>{
 module.exports.getDocuments = async(user)=>{
     //get documents for course and year from user info
     try {
-        const documents  = await resourcesModel.find({
-            course:user.course,
-            year:user.year
-        });
+        let documents  = [];
+        if(user.lecture){
+            documents = await resourcesModel.find()
+            .sort({ createdAt: -1 }) // Sort by createdAt in descending order (latest first)
+            .limit(20);
+        }else{
+          documents = await resourcesModel.find({
+                course:user.course,
+                year:user.year
+            });
+        }
+
         return documents
     } catch (error) {
      
@@ -44,3 +52,30 @@ module.exports.getDocuments = async(user)=>{
     }
 
 }
+
+
+module.exports.deleteDocument = async (req, res) => {
+    try {
+        const user = req.user;
+        const { documentId } = req.params; // Get documentId from the URL parameter
+
+        // Ensure that the user is the owner of the document (by checking the 'user' field)
+        const documentToDelete = await resourcesModel.findOne({ user, _id: documentId });
+
+        if (!documentToDelete) {
+            // If the document doesn't exist or doesn't belong to the user, return an error message
+            return res.status(404).json({ success: false, message: 'Document not found or unauthorized.' });
+        }
+
+        // Delete the document
+        await resourcesModel.deleteOne({ _id: documentId });
+
+        // Return a success message
+        return res.status(200).json({ success: true, message: 'Document deleted successfully.' });
+    } catch (error) {
+        console.error(error);
+        // Return an error message in case of an exception
+        return res.status(500).json({ success: false, message: 'An error occurred while deleting the document.' });
+    }
+};
+
--- router/resources.js
+++ router/resources.js
@@ -6,5 +6,7 @@ const {upload} = require("../middleware/upload");
 
 router.get('/',verifyToken,resourcesController.resources);
 router.post('/',verifyToken,upload,resourcesController.uploadDocument);
+router.delete('/:documentId', verifyToken, resourcesController.deleteDocument);
+
 
 module.exports = router
