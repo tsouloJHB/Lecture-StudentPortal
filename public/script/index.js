821ea2a12e714d5554f55af5941ae6c365bd6e8c
soulo <tsoulo@student.wethinkcode.co.za>
2023-09-02 20:03:58 +0200

    message

--- public/script/index.js
+++ public/script/index.js
@@ -33,11 +33,80 @@ socket.on("get-users", activeUsers => {
     console.log(localActiveUsers);
 });
 
+
 socket.on("receive-message",message =>{
     console.log(message);
+    displayMessage(message,false);
 });
+
+
+
 socket.on("remove-user", userToRemove => {
     // Filter localActiveUsers to exclude the user with a matching socketId
     localActiveUsers = localActiveUsers.filter(user => user.socketId !== userToRemove);
     console.log(localActiveUsers);
 });
+
+ async function socketSendMessage(message){
+    
+    // Create an array of receiver IDs from localActiveUsers
+    const receiverIds = localActiveUsers.map(activeUser => activeUser.userId);
+
+    // Broadcast message data
+    const data = {
+        receiverIds: receiverIds,
+        data: message
+    };
+   socket.emit("broadcast message",data);
+    
+}
+
+function formatTime(timestamp) {
+    const date = new Date(timestamp);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours}:${minutes}`;
+}
+
+function displayMessage(message,sender){
+    // Get the current time
+    const currentTime = new Date();
+
+    // Format the current time as a string (e.g., "HH:mm")
+    const formattedTime = `${currentTime.getHours()}:${currentTime.getMinutes()}`;
+
+    // Create a new <li> element for the received message with the formatted time
+    const messageListItem = document.createElement("li");
+
+    // Check if the message is from the current user or sender
+   
+    if (sender) {
+        messageListItem.classList.add("repaly");
+    } else {
+        messageListItem.classList.add("sender");
+    }
+  
+
+    // Set the message content and formatted time
+    messageListItem.innerHTML = `
+        <p>${message}</p>
+        <span class="time">${formattedTime}</span>
+    `;
+
+    // Append the message element to the message list
+    const messageList = document.querySelector(".msg-body ul");
+    messageList.appendChild(messageListItem);
+
+    // Optionally, scroll to the bottom of the message list to show the latest message
+    messageList.scrollTop = messageList.scrollHeight;
+}
+
+
+// const messageButton = document.getElementById('sendButton');
+// messageButton.addEventListener('click', async function() {
+//       var messageText = $("#messageInput").val();
+//     await socketSendMessage(messageText);
+// });
+
+window.displayMessage = displayMessage;
+window.socketSendMessage = socketSendMessage;
