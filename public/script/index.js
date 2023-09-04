22f026a1ba0bff1aeec8e88e532797728405a906
soulo <tsoulo@student.wethinkcode.co.za>
2023-09-04 21:58:18 +0200

    Config fix2

--- public/script/index.js
+++ public/script/index.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client'
-const socket = io("http://localhost:8080");
+const socket = io("https://student-lecture.onrender.com");
+//const socket = io("http://localhost:8080");
 
 // const button = document.getElementById('myButton');
 
