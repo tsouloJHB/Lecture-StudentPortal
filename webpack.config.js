dcfac0fe7ea724a4caa2ce3ee7904c102e1b5af2
soulo <tsoulo@student.wethinkcode.co.za>
2023-09-04 22:17:02 +0200

    web pack fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development', // or 'production'
+    mode: 'production', // or 'production'
     entry: './public/script/index.js', // Entry point of your client-side code
     output: {
         filename: 'bundle.js', // Output bundle file
         path: path.resolve(__dirname, 'public/dist'), // Output directory
+        publicPath: '/',
     },
 };
